9a12ee030fcca1a65a8dbc547f3e80dc8a355751
Jaf Crisologo <jafcrisologo@Jafs-MacBook-Pro.local>
2016-04-30 02:00:21 -0600

    Added a details ViewController

--- section7-pokedex/PokemonDetailVC.swift
+++ section7-pokedex/PokemonDetailVC.swift
@@ -9,8 +9,20 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
     var pokemon: Pokemon! //can't use this screen without a Pokemon object ready to intercept whatever's passed from the main storyboard
     
     override func viewDidLoad() {
@@ -24,15 +36,8 @@ class PokemonDetailVC: UIViewController {
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
 
 }
